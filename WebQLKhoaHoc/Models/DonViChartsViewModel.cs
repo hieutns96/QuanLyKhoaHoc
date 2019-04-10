52379b9d498a080d94be6c8c4fb52cfe63a695a9
Nguyen Hoang Anh Tu <nguyenhoanganhtu2406@gmail.com>
2019-04-10 22:59:13 +0700

    DonVi Charts (continued)

---
+++ WebQLKhoaHoc/Models/DonViChartsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebQLKhoaHoc.Models
+{
+    public class DonViChartsViewModel
+    {
+        public List<Object> Header { get; set; } = new List<Object>();
+        public List<List<Object>> Rows { get; set; } = new List<List<Object>>();
+    }
+}
