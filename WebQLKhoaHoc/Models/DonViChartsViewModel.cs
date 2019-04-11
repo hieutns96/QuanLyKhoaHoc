eb83d11bec5216a04bce5f2bf2c303a657520381
Hieu-Tran <hieu.tran@watasolutions.com>
2019-04-11 19:36:16 +0700

    Fix Layout

--- WebQLKhoaHoc/Models/DonViChartsViewModel.cs
+++ WebQLKhoaHoc/Models/DonViChartsViewModel.cs
@@ -7,7 +7,13 @@ namespace WebQLKhoaHoc.Models
 {
     public class DonViChartsViewModel
     {
-        public List<Object> Header { get; set; } = new List<Object>();
-        public List<List<Object>> Rows { get; set; } = new List<List<Object>>();
+        public List<Object> Header { get; set; } 
+        public List<List<Object>> Rows { get; set; } 
+
+        public DonViChartsViewModel()
+        {
+            this.Header = new List<Object>();
+            this.Rows = new List<List<Object>>();
+        }
     }
 }
