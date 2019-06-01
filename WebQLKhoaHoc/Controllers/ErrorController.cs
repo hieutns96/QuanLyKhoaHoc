830b622bb88d38eb47002dc6d1dad8f5a0c17d25
Hieu-Tran <hieu.tran@watasolutions.com>
2019-06-01 11:39:08 +0700

    Fix Error Page

---
+++ WebQLKhoaHoc/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebQLKhoaHoc.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult NotFound()
+        {
+           Response.StatusCode = 404;  //you may want to set this to 200
+            var error = new HandleErrorInfo(new Exception("Trang không tồn tại"), "ErrorController","NotFound");
+            return View(error);
+        }
+    }
+}
